8aa57fde3e830af00d461fcf351e7febf52cdacb
Nicole Cheng <nicole_t_cheng@dlsu.edu.ph>
2021-08-24 16:26:49 +0800

    select screen

--- app/src/main/java/com/mobdeve/s12/cheng/delacruz/palettelock/StartScreen.java
+++ app/src/main/java/com/mobdeve/s12/cheng/delacruz/palettelock/StartScreen.java
@@ -1,6 +1,7 @@
 package com.mobdeve.s12.cheng.delacruz.palettelock;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -14,10 +15,11 @@ import android.view.WindowManager;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class StartScreen extends AppCompatActivity {
 
-    private Rect mRectSlot1;
-    private Paint mPaintSlot1;
+    LottieAnimationView lottiePlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,17 @@ public class StartScreen extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start_screen);
+
+        lottiePlay = findViewById(R.id.lottie_play);
+
+        lottiePlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (StartScreen.this, SelectLevel.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 
